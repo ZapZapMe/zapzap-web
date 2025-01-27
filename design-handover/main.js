69db390ae18c93efffe369ad9cfd1ce865e9ab5d
Simon Tennant <simon@imaginator.com>
2025-01-27 12:09:37 +0100

    pre-pairing push

---
+++ design-handover/main.js
@@ -0,0 +1,30 @@
+console.log(`main.js`)
+
+const tipTweetURLInput = document.querySelector(`.tipCard input.tipTweetURLInput`)
+const tipTweetURLErrorMsg = document.querySelector(`.tipTweetURLErrorMsg`)
+const tipTweetURLButton = document.querySelector(`button.tipTweetURLButton`)
+
+tipTweetURLInput.addEventListener('blur', () => {
+	const regex = /^(https?:\/\/)?(www\.)?x\.com\/user\/status\/\d+$/
+
+	if (tipTweetURLInput.value) {
+		if (!regex.test(tipTweetURLInput.value)) {
+			tipTweetURLInput.setCustomValidity('')
+			tipTweetURLInput.classList.add('invalid')
+			tipTweetURLErrorMsg.classList.remove('hide')
+			tipTweetURLButton.disabled = true
+		}
+
+		else {
+			tipTweetURLInput.classList.remove('invalid')
+			tipTweetURLErrorMsg.classList.add('hide')
+			tipTweetURLButton.disabled = false
+		}
+	}
+
+	else {
+		tipTweetURLInput.classList.remove('invalid')
+		tipTweetURLErrorMsg.classList.add('hide')
+		tipTweetURLButton.disabled = true
+	}
+})
