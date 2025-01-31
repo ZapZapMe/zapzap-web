68fa57df2639289efb7bd440d983b6bdd0b92973
Simon Tennant <simon@imaginator.com>
2025-01-31 19:00:55 +0100

    use default avatar

--- src/components/UserProfilePage/ProfileFeed.jsx
+++ src/components/UserProfilePage/ProfileFeed.jsx
@@ -2,13 +2,40 @@ import React from 'react';
 import FeedItem from './FeedItem';
 
 const FEED_ITEMS = [
-  { profilePic: "img/dp5.png", username: "someone_else", amount: "4,000", timeAgo: "Just now" },
-  { profilePic: "img/dp1.png", username: "short", amount: "5,000", timeAgo: "2 hours ago", comment: "Tip 4 u" },
-  { profilePic: "img/dp2.png", username: "very_very_looong_username", amount: "1,000,000", timeAgo: "4 days ago",
-    comment: "Very long tip message for you just you. Looooooooong tip message for a big tip from me to you." },
-  { profilePic: "img/dp3.png", username: "normalguy", amount: "10,000", timeAgo: "2 weeks ago",
-    comment: "Just a normal guy sending you a normal tip." },
-  { profilePic: "img/dp4.png", username: "elonmusk", amount: "4,000", timeAgo: "1 year ago" }
+  {
+    profilePic: 'img/default-avatar.png',
+    username: 'someone_else',
+    amount: '4,000',
+    timeAgo: 'Just now',
+  },
+  {
+    profilePic: 'img/default-avatar.png',
+    username: 'short',
+    amount: '5,000',
+    timeAgo: '2 hours ago',
+    comment: 'Tip 4 u',
+  },
+  {
+    profilePic: 'img/default-avatar.png',
+    username: 'very_very_looong_username',
+    amount: '1,000,000',
+    timeAgo: '4 days ago',
+    comment:
+      'Very long tip message for you just you. Looooooooong tip message for a big tip from me to you.',
+  },
+  {
+    profilePic: 'img/default-avatar.png',
+    username: 'normalguy',
+    amount: '10,000',
+    timeAgo: '2 weeks ago',
+    comment: 'Just a normal guy sending you a normal tip.',
+  },
+  {
+    profilePic: 'img/default-avatar.png',
+    username: 'elonmusk',
+    amount: '4,000',
+    timeAgo: '1 year ago',
+  },
 ];
 
 const ProfileFeed = () => (
