681f70c951bc79b835fc9dd07d60d6f8ca9e73ac
Zain Ali Shah <zainalishah51472@gmail.com>
2025-02-06 17:41:17 +0300

    updated to include trailing / in tips

--- src/lib/utils/apiHandlers.ts
+++ src/lib/utils/apiHandlers.ts
@@ -3,60 +3,57 @@ import axiosClient, { API_BASE_URL } from './axiosClient';
 
 // Example API handler
 // PUT /users/me
-export const updateWalletAddress = async (newAddress:string) => {
+export const updateWalletAddress = async (newAddress: string) => {
   try {
-    const response = await axiosClient.put(`${API_BASE_URL}/users/me`, { wallet_address: newAddress });
+    const response = await axiosClient.put(`${API_BASE_URL}/users/me`, {
+      wallet_address: newAddress,
+    });
     return response.data;
   } catch (error) {
     throw error;
   }
 };
 
-
 type TCreateInvoice = {
-  "amount_sats": number,
-  "comment": string,
-  "tweet_url": string,
-  "tip_sender": string
-}
+  amount_sats: number;
+  comment: string;
+  tweet_url: string;
+  tip_sender: string;
+};
 // POST /tips
-export const createInvoice = async (request_body:TCreateInvoice):Promise<AxiosResponse<any, any>> =>{
+export const createInvoice = async (
+  request_body: TCreateInvoice
+): Promise<AxiosResponse<any, any>> => {
   try {
-    return await axiosClient.post(`${API_BASE_URL}/tips`, request_body);
-    
+    return await axiosClient.post(`${API_BASE_URL}/tips/`, request_body);
   } catch (error) {
     throw error;
-    
   }
-}
+};
 
 // GET tips => /tips/received/:username
-export const getUsersTipReceived = async (username:string) =>{
+export const getUsersTipReceived = async (username: string) => {
   try {
-    return await axiosClient.get(`${API_BASE_URL}/tips/received/${username}`); 
+    return await axiosClient.get(`${API_BASE_URL}/tips/received/${username}`);
   } catch (error) {
     throw error;
-    
   }
-}
+};
 
 // GET tips => /tips/received/:username
-export const getUsersTipSent = async (username:string) =>{
+export const getUsersTipSent = async (username: string) => {
   try {
-    return await axiosClient.get(`${API_BASE_URL}/tips/sent/${username}`); 
+    return await axiosClient.get(`${API_BASE_URL}/tips/sent/${username}`);
   } catch (error) {
     throw error;
-    
   }
-}
+};
 
-
-// GET USER BY USERNAME => /users/:username 
-export const getUserByUsername = async(username:string)=>{
+// GET USER BY USERNAME => /users/:username
+export const getUserByUsername = async (username: string) => {
   try {
-    return await axiosClient.get(`${API_BASE_URL}/users/${username}`); 
+    return await axiosClient.get(`${API_BASE_URL}/users/${username}`);
   } catch (error) {
     throw error;
-    
   }
-}
+};
