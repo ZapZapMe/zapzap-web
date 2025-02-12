4fb9b3cd23920929e09fd2613f709ebd4ea197b7
Yan <tiervirov@gmail.com>
2025-02-12 12:40:11 +0200

    chore: testing commitlint rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,23 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+      ],
+    ],
+    'body-max-line-length': [2, 'always', 'Infinity'],
+  },
+};
