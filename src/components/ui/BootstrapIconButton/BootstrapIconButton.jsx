a558e54f47a43f28271588c8bd5bac3208851b63
Yan <tiervirov@gmail.com>
2025-02-18 08:25:49 +0200

    fix(button): styles propagation

--- src/components/ui/BootstrapIconButton/BootstrapIconButton.jsx
+++ src/components/ui/BootstrapIconButton/BootstrapIconButton.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import './BootstrapIconButton.scss';
 
-const BootstrapIconButton = ({ icon, onClick, children, ...props }) => {
+const BootstrapIconButton = ({
+  icon,
+  onClick,
+  children,
+  className,
+  ...props
+}) => {
   return (
-    <Button className="zz-custom" onClick={onClick} {...props}>
+    <Button className={`zz-custom ${className}`} onClick={onClick} {...props}>
       {icon}
       {children}
     </Button>
